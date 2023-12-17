20757a0c709db3f452ccbe868fc113a4a315c9b1
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2023-12-17 14:46:22 +0300

    WIP (ML)

--- frontend/src/app/services/authWithSocial.ts
+++ frontend/src/app/services/authWithSocial.ts
@@ -1,24 +1,26 @@
 import axios from "axios";
 
 export type VkLoginRequest = {
-  code: string;
-}
+    code: string;
+};
 
 export type VkLoginResponse = {
-  access_token: string;
-  token_type: string;
-}
+    access_token: string;
+    token_type: string;
+};
 
 class VkService {
-  static async loginVk(code: VkLoginRequest): Promise<VkLoginResponse> {
-    try {
-      const response = await axios.post<VkLoginResponse>(`https://larek.itatmisis.ru/api/auth/vk?code=${code}`);
-      console.log(response.data)
-      return response.data;
-    } catch (error) {
-      throw new Error("Error while making the request");
+    static async loginVk(code: VkLoginRequest): Promise<VkLoginResponse> {
+        try {
+            const response = await axios.post<VkLoginResponse>(
+                `http://localhost:8000/api/auth/vk?code=${code}`,
+            );
+            console.log(response.data);
+            return response.data;
+        } catch (error) {
+            throw new Error("Error while making the request");
+        }
     }
-  }
 }
 
 export default VkService;
