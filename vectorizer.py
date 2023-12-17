8cabba2c1c126715914ab8e3e9d3ed1999ac7f8b
Георгий Апарин <70803676+Egorgij21@users.noreply.github.com>
2023-12-17 15:30:30 +0300

    Update vectorizer.py

--- vectorizer.py
+++ vectorizer.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import AutoTokenizer, AutoModel
 from typing import List
+import re
 
 tokenizer = AutoTokenizer.from_pretrained("cointegrated/rubert-tiny2")
 sbert = AutoModel.from_pretrained("cointegrated/rubert-tiny2")
@@ -8,6 +9,16 @@ sbert = AutoModel.from_pretrained("cointegrated/rubert-tiny2")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 sbert = sbert.to(device)
 sbert = sbert.eval()
+
+emoji_pattern = re.compile('[\U00010000-\U0010ffff]', flags=re.UNICODE)
+
+def clean(x):
+    x = re.sub("<br>", ' ', x)
+    x = re.sub("\n", ' ', x)
+    x = emoji_pattern.sub('', x)
+    x = re.sub(r'[abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ]', r' ', x)
+    x = re.sub(r'(http\S+)|(www\S+)|([\w\d]+www\S+)|([\w\d]+http\S+)', r' ', x)
+    return x
 
 
 def mean_pooling(model_output, attention_mask):
@@ -19,7 +30,8 @@ def mean_pooling(model_output, attention_mask):
 
 
 def vectorize(text: str) -> List[float]:
-    encoded_input = tokenizer(text, padding=True, truncation=True, max_length=64, return_tensors='pt')
+    text = clean(text)
+    encoded_input = tokenizer(text, padding=True, truncation=True, max_length=256, return_tensors='pt')
     with torch.no_grad():
         model_output = sbert(**encoded_input)
     sentence_embeddings = mean_pooling(model_output, encoded_input['attention_mask'])
