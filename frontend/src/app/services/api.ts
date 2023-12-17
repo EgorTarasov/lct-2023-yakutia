8417e648d595bc80263b52024e566db4d2f7785d
Aidar <abdykayimovaidar@gmail.com>
2023-12-17 23:46:48 +0300

    Refactor API service and update endpoints

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -1,56 +1,59 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { RootState } from "../store";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { RootState } from '../store'
 
 export type User = {
-    first_name: string;
-    last_name: string;
-    username: string;
-    email: string;
-    password: string;
-};
+  first_name: string
+  last_name: string
+  username: string
+  email: string
+  password: string
+
+}
 
 export interface UserResponse {
-    access_token: string;
+  access_token: string
 }
 
 export type SignUpRequest = {
-    first_name: string;
-    last_name: string;
-    email: string;
-    password: string;
-};
+  first_name: string
+  last_name: string
+  email: string
+  password: string
+}
+
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:8000/api/",
-        prepareHeaders: (headers, { getState }) => {
-            const token = (getState() as RootState).authSlice.token;
-            if (token) {
-                headers.set("authorization", `Bearer ${token}`);
-            }
-            return headers;
-        },
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://larek.itatmisis.ru/api/',
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).authSlice.token
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`)
+      }
+      return headers
+    },
+  }),
+  endpoints: (builder) => ({
+
+    // AUTH
+
+    login: builder.mutation<UserResponse, FormData>({
+      query: (credentials) => ({
+        url: 'auth/login',
+        method: 'POST',
+        body: credentials,
+      }),
     }),
-    endpoints: (builder) => ({
-        // AUTH
 
-        login: builder.mutation<UserResponse, FormData>({
-            query: (credentials) => ({
-                url: "auth/login",
-                method: "POST",
-                body: credentials,
-            }),
-        }),
+    signUp: builder.mutation<UserResponse, SignUpRequest>({
+      query: (credentials) => ({
+        url: 'auth/register',
+        method: 'POST',
+        body: credentials,
+      }),
+    }),
 
-        signUp: builder.mutation<UserResponse, SignUpRequest>({
-            query: (credentials) => ({
-                url: "auth/register",
-                method: "POST",
-                body: credentials,
-            }),
-        }),
-
-        // USER
+    // USER
 
     getPredictions: builder.query<{ message: string }, void>({
       query: () => 'user/pred',
@@ -61,11 +64,10 @@ export const api = createApi({
     }),
 
 
-        // PROFESSION
+    // PROFESSION
 
-        getProfessions: builder.query<{ message: string }, void>({
-            query: () => "profession/all",
-        }),
+    getProfessions: builder.query<{ message: string }, void>({
+      query: () => 'profession/all',
     }),
 
     getProfessionById: builder.query<{ id: number, name: string, descriptions: { description: string }[] }, number>({
